f65eb9815d91e77ea1e2983026c57252ee28188c
Bianca Lupu <biancawo003@gmail.com>
2025-09-06 13:00:17 +0300

    workshop: calcul IMC

---
+++ 08_JS_basics/05_conditionals/01_indice_masa_corporala/app.js
@@ -0,0 +1,43 @@
+var inputInaltime = document.getElementById("inaltime");
+var inputGreutate = document.getElementById("greutate");
+var paragrafRezultat = document.getElementById("output");
+
+function calculeazaIMC() {
+    
+    var inaltime = Number(inputInaltime.value);
+    var greutate = Number(inputGreutate.value);
+    var IMC = greutate / (inaltime * inaltime);
+
+
+
+    if (IMC < 18.5) {
+        paragrafRezultat.innerHTML = "IMC-ul tău este " + IMC.toFixed(2) + "--" + " Risc pentru sănătate: ridicat Această valoare indică riscuri pentru sănătatea ta, este mult prea mică pentru o sănătate optimă.";
+    } else if (IMC >= 18.5 && IMC <= 24.9) {
+        paragrafRezultat.innerHTML =
+          "IMC-ul tău este " +
+          IMC.toFixed(2) +
+          "--" +
+          " Risc pentru sănătate: minim/scăzut";
+    } else if (IMC >= 25 && IMC <= 29.9) {
+        paragrafRezultat.innerHTML =
+          "IMC-ul tău este " +
+          IMC.toFixed(2) +
+          "--" +
+          " Risc pentru sănătate: scăzut/moderat";
+    } else if (IMC >= 30 && IMC <= 34.9) {
+        paragrafRezultat.innerHTML =
+          "IMC-ul tău este " +
+          IMC.toFixed(2) +
+          "--" +
+          " Risc pentru sănătate: moderat/ridicat Acesta e un semn de avertisment: schimbă-ți dieta acum!";
+    }else {
+        paragrafRezultat.innerHTML =
+          "IMC-ul tău este " +
+          IMC.toFixed(2) +
+          "--" +
+          " Risc pentru sănătate: ridicat Greutatea iți afectează în mod radical sănătatea. Scapă de surplusul de greutate!";
+    }
+ 
+
+    console.log(IMC);
+}
